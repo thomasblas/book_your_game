feb93d41ec827ce5874836b1534352258374dea7
Christian Huang <christian.huang.49@gmail.com>
2021-06-15 11:26:05 +0200

    User Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.all.destroy_all
+User.create!(email: "admin@email.com", nickname: "Admin", address: "165 avenue de Bretagne", password: "123456")
+User.create!(email: "christian@email.com", nickname: "Christian", address: "165 avenue de Bretagne", password: "123456")
+User.create!(email: "thom@email.com", nickname: "Thom", address: "SDF", password: "123456")
+User.create!(email: "thomas@email.com", nickname: "Thomas", address: "1 Rue de Normandie", password: "123456")
