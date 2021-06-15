8dded00fc6f88bee6e4ba505f61b27211ae8f212
Christian Huang <christian.huang.49@gmail.com>
2021-06-15 16:36:09 +0200

    Fix Devise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
   def home
     @video_games = VideoGame.all
     @homepage_video_games = @video_games.sample(3)
