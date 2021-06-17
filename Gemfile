0aefc6a4e9e28addea4846071b040d80548d3ba2
Thomas Petitprez <t.petitprez@outlook.fr>
2021-06-17 13:36:51 +0200

    cloudinary

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'pundit'
 
 gem 'pg_search', '~> 2.3.0'
 
+gem 'cloudinary', '~> 1.16.0'
+
 group :development, :test do
   gem 'pry-byebug'
   gem 'pry-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :video_games, dependent: :destroy
   has_many :rents, dependent: :destroy
+  has_one_attached :photo
 end
